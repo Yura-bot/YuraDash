aef1d8891a594b29526cd30a18dc6f055e022a4b
Pronicio <paolo0803.giometti@gmail.com>
2021-08-29 18:09:15 +0200

    🎈 Ajout du template.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "@/assets/css/libs.min.css"
+import "@/assets/css/hope-ui.css?v=1.0.2"
+
 createApp(App).use(router).mount('#app')
