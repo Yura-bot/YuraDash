c93267140c77f1814add16e543c304a9754b0a52
Pronicio <52421988+Pronicio@users.noreply.github.com>
2021-08-30 17:03:01 +0200

    🤺 Connexion à l'api.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import router from './router'
+import store from './store'
 
-import "@/assets/css/libs.min.css"
-import "@/assets/css/hope-ui.css?v=1.0.2"
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(VueAxios, axios)
+    .mount('#app')
